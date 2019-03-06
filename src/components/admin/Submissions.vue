83d4ef96534ef9f9519c95f90053a2634d68cdfe
Rex <rex@rex.gs>
2019-03-06 16:11:53 +0900

    fixed layouts of admin submission page

--- src/components/admin/Submissions.vue
+++ src/components/admin/Submissions.vue
@@ -2,43 +2,45 @@
   <div>
     <div class="container">
       <h3>Submissions</h3>
-      <div class="row form-inline">
-        <div class="col-md-4">
-          <div class="form-group">
-            <label for="team">Team:&nbsp;</label>
-
-            <input type="text" class="form-control" id="team" placeholder="Team Name" v-model="filter.team">
-          </div>
-        </div>
-        <div class="col-md-5">
-          <div class="form-group">
-            <label for="challenge">Challenge:&nbsp;</label>
-
-            <input type="text" class="form-control" id="challenge" placeholder="Challenge Name" v-model="filter.challenge">
-          </div>
-        </div>
-        <div class="col-md-3">
-          <div class="form-group">
-            <label for="challenge">Correct:&nbsp;</label>
-
-            <b-form-select v-model="filter.is_correct">
-              <option :value="null"></option>
-              <option :value="true">Correct</option>
-              <option :value="false">Incorrect</option>
-            </b-form-select>
-          </div>
+      <div class="form-group row">
+        <label for="team" class="col-sm-2 col-form-label">Team:</label>
+        <div class="col-sm-4">
+          <input type="text" class="form-control" id="team" placeholder="Team Name" v-model="filter.team">
         </div>
       </div>
-      <div class="row">
-        <b-table :items="submissions"
-                 :fields="submissions_fields"
-                 :per-page="per_page"
-                 :current-page="current_page"
-                 sort-by="submitted_at"
-                 :sort-desc="true"
-                 :filter-function="apply_filter"
-                 :filter="filter"
-                 @filtered="onfiltered"
+      <div class="form-group row">
+        <label for="challenge" class="col-sm-2 col-form-label">Challenge:</label>
+        <div class="col-sm-4">
+          <input type="text" class="form-control" id="challenge" placeholder="Challenge Name" v-model="filter.challenge">
+        </div>
+      </div>
+      <div class="form-group row">
+        <label for="challenge" class="col-sm-2 col-form-label">Correct:</label>
+        <div class="col-sm-4">
+          <b-form-radio-group
+            buttons
+            v-model="filter.is_correct"
+            button-variant="outline-primary"
+          >
+            <b-form-radio :value="null">All</b-form-radio>
+            <b-form-radio :value="true" button-variant="outline-success">Correct</b-form-radio>
+            <b-form-radio :value="false" button-variant="outline-danger">Incorrect</b-form-radio>
+          </b-form-radio-group>
+        </div>
+      </div>
+    </div>
+    <div class="container">
+      <b-table hover
+               :items="submissions"
+               :fields="submissions_fields"
+               :per-page="per_page"
+               :current-page="current_page"
+               :tbody-tr-class="row_class"
+               sort-by="submitted_at"
+               :sort-desc="true"
+               :filter-function="apply_filter"
+               :filter="filter"
+               @filtered="onfiltered"
         >
           <template slot="team" slot-scope="scope">
             {{ scope.value.username }}
@@ -52,11 +54,13 @@
         </b-table>
       </div>
       <div class="row">
-        <b-pagination
-          :per-page="per_page"
-          :total-rows="total_rows"
-          v-model="current_page">
-        </b-pagination>
+        <div class="mx-auto">
+          <b-pagination
+            :per-page="per_page"
+            :total-rows="total_rows"
+            v-model="current_page">
+          </b-pagination>
+        </div>
       </div>
     </div>
   </div>
@@ -107,6 +111,13 @@ export default {
     onfiltered: function(items) {
       this.total_rows = items.length;
       this.current_page = 1;
+    },
+    row_class: function(item) {
+      if (item.is_correct) {
+        return 'table-success';
+      } else {
+        return 'table-danger';
+      }
     }
   }
 }
